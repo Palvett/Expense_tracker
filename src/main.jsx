b4475256076835c41f3ada4b4ab65b7d4820d549
Palvett Blaise <blaisepalvett@gmail.com>
2026-08-30 19:34:01 +0100

    CategoryContext, CategoryProvider seeded with defaults, useCategories hook. Had to split Category context into context, provider and hook files to satisfy fast refresh

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { TransactionProvider } from './context/TransactionContext.jsx'
+import { CategoryProvider } from './context/CategoryContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <TransactionProvider>
-        <App />
-      </TransactionProvider>
+      <CategoryProvider>
+        <TransactionProvider>
+          <App />
+        </TransactionProvider>
+      </CategoryProvider>
     </BrowserRouter>  
   </StrictMode>,
 )
