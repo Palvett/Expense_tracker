1adcb700979b372e8130b007a6ed36a8675fceae
Palvett Blaise <blaisepalvett@gmail.com>
2026-09-01 20:37:51 +0100

    Built a color pallet that shows category color and name on each tansaction item

--- src/features/transactions/TransactionItem.jsx
+++ src/features/transactions/TransactionItem.jsx
@@ -1,18 +1,31 @@
 import { formatCurrency } from "../../utils/formatCurrency";
 import { TRANSACTION_TYPES } from "../../utils/constants";
+import { useCategories } from '../../hooks/useCategories'
 
 function TransactionItem({ transaction }) {
+    const { categories } = useCategories()
     const isIncome = transaction.type === TRANSACTION_TYPES.INCOME
+    const category = categories.find((c) => c.id === transaction.categoryId)
 
   return (
     <div className="transaction-item">
+      <div className="transaction-left">
+        <span
+          className="category-dot"
+          style={{ backgroundColor: category?.color ?? '#ccc' }}
+        />
         <div className="transaction-info">
-            <span className="transaction-date">{transaction.date}</span>
-            <span clsasName="transaction-note">{transaction.note || 'No note'}</span>
+          <span className="transaction-category">{category?.name ?? 'Uncategorized'}</span>
+          <span className="transaction-date">{transaction.date}</span>
         </div>
+      </div>
+
+      <div className="transaction-right">
+        <span className="transaction-note">{transaction.note || 'No note'}</span>
         <span className={`transaction-amount ${isIncome ? 'income' : 'expense'}`}>
-            {isIncome ? '+' : '-'} {formatCurrency(transaction.amount)}
+          {isIncome ? '+' : '-'} {formatCurrency(transaction.amount)}
         </span>
+      </div>
     </div>
   )
 }
