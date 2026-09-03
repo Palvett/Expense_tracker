54c27fca187e7df4b05f5c0b1dd0d8afeb6438fb
Palvett Blaise <blaisepalvett@gmail.com>
2026-09-03 17:49:52 +0100

    BudgetForm with category/month/limit input fieds and duplicate budgetvalidation

--- src/pages/Budgets.jsx
+++ src/pages/Budgets.jsx
@@ -1,7 +1,12 @@
+import BudgetForm from "../features/budgets/BudgetForm"
+
 function Budgets() {
   return (
     <div>
       <h1 className="page-title">Budgets</h1>
+      <div style={{width: '70%', margin: '0 auto'}}>
+        <BudgetForm />
+      </div>
     </div>
   )
 }
