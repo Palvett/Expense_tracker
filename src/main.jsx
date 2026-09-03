ba8eda3b0a3a094bdbfde2385bc75a68494765a8
Palvett Blaise <blaisepalvett@gmail.com>
2026-09-03 16:14:21 +0100

    Budget context, BadgeProvider, and useBudgets hook, localStorage-backed

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import './index.css'
 import App from './App.jsx'
 import { TransactionProvider } from './context/TransactionContext.jsx'
 import { CategoryProvider } from './context/CategoryContext.jsx'
+import { BudgetProvider } from './context/BudgetProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <CategoryProvider>
         <TransactionProvider>
-          <App />
+          <BudgetProvider>
+            <App />
+          </BudgetProvider>
         </TransactionProvider>
       </CategoryProvider>
     </BrowserRouter>  
