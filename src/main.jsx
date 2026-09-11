7ee8d1b3b97771a4c57672c08ad14fc07ce3bf31
Palvett Blaise <blaisepalvett@gmail.com>
2026-09-11 15:26:46 +0100

    Added switch color picker swatches responsively on very narrow screens to prevent overflow

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from './context/ThemeProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Expense_tracker">
       <ThemeProvider>
         <CategoryProvider>
           <TransactionProvider>
