17a0bcdeddd70edc6113c11d6e476f295314b27b
Palvett Blaise <blaisepalvett@gmail.com>
2026-09-12 18:23:03 +0100

    fix: remove router basename for vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from './context/ThemeProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Expense_tracker">
+    <BrowserRouter>
       <ThemeProvider>
         <CategoryProvider>
           <TransactionProvider>
